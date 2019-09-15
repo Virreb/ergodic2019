97d5c5da04065804dba9295468585cf37efa79a9
Victor Bäckman <victorbackman2@gmail.com>
2019-09-15 17:56:02 +0200

    plot functions

---
+++ plot.py
@@ -0,0 +1,48 @@
+
+
+def show_image(base, name, predicted=None):
+
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    from scipy.ndimage.interpolation import rotate
+    import numpy as np
+
+    # load original image
+    image_path = base + '/Images/' + name + '.jpg'
+    img = mpimg.imread(image_path)
+
+    # load mask
+    mask_path = base + '/Masks/all/' + name + '.png'
+    mask = mpimg.imread(mask_path)
+
+    # prediction
+    # predicted = rotate(mask, angle=2)
+    # predicted = mask + 5
+    predicted = mask
+
+    diff = predicted - mask
+
+    fig = plt.figure()
+    ax_mask = fig.add_subplot(2, 2, 1)
+    ax_pred = fig.add_subplot(2, 2, 2)
+    ax_diff = fig.add_subplot(2, 2, 3)
+    ax_raw = fig.add_subplot(2, 2, 4)
+
+    ax_mask.imshow(img)
+    ax_mask.imshow(mask, alpha=0.4)
+    ax_mask.title.set_text('Mask')
+
+    ax_pred.imshow(img)
+    ax_pred.imshow(predicted, alpha=0.4)
+    ax_pred.title.set_text('Predicted')
+
+    ax_diff.imshow(img)
+    ax_diff.imshow(diff, alpha=0.4)
+    ax_diff.title.set_text('Diff')
+
+    ax_raw.imshow(img)
+    ax_raw.title.set_text('Original')
+
+    plt.show()
+
+show_image(base='data/raw', name='coxs_1_02_02')
