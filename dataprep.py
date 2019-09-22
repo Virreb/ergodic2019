f8aa58fc14312797cbeb4cd50b84e75ea17f59b9
isakhjortgren <isakhjortgren@gmail.com>
2019-09-22 11:58:21 +0200

    ninja loader

--- dataprep.py
+++ dataprep.py
@@ -1,4 +1,10 @@
+import torch
 from torch.utils.data import Dataset
+
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+import imageio
 
 
 class GLOBHEDataset(Dataset):
@@ -13,10 +19,9 @@ class GLOBHEDataset(Dataset):
         return len(self.image_names)
 
     def __getitem__(self, idx):
-        import imageio
         sample = {
-            'image': imageio.imread(self.image_paths[idx]),
-            'bitmap': imageio.imread(self.bitmap_paths[idx]),
+            'image': Image.open(self.image_paths[idx]),
+            'bitmap': Image.open(self.bitmap_paths[idx]),
         }
 
         if self.transform is not None:
@@ -25,3 +30,39 @@ class GLOBHEDataset(Dataset):
         return sample
 
 
+class ToTensor:
+    def __init__(self):
+        self._built_in_to_tensor = transforms.ToTensor()
+
+    def __call__(self, sample):
+        image = sample['image']
+        bitmap = sample['bitmap']
+
+        return {'image': self._built_in_to_tensor(image),
+                'bitmap': (255*self._built_in_to_tensor(bitmap)).int()}
+
+
+class Resize:
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+        self._built_in_resize = transforms.Resize(self.output_size, interpolation=0)
+
+    def __call__(self, sample):
+        image = sample['image']
+        bitmap = sample['bitmap']
+
+        img = self._built_in_resize(image)
+        bmp = self._built_in_resize(bitmap)
+
+        return {'image': img, 'bitmap': bmp}
+
+dataset = GLOBHEDataset('train')
+test = dataset[0]
+resize = Resize((512, 512))
+test_1 = resize(test)
+
+
+to_tensor = ToTensor()
+test_2 = to_tensor(test_1)
+print('hej')
